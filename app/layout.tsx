8280aa503b3d230f19150733d0f5311b13b547b8
gulifan <gulifan@dxy.cn>
2023-12-20 00:21:12 +0800

    feat: 接入supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import './globals.css'
-import Sidebar from '@/components/Sidebar'
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'spotify clone ',
-  description: 'listen to music',
-}
+  title: "spotify clone ",
+  description: "listen to music",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
-  )
+  );
 }
