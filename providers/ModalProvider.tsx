674b813fc36e44dac5721e28656bc13a58a1781a
gulifan <gulifan@dxy.cn>
2023-12-28 15:09:42 +0800

    feat: modal 组件

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Modal from "@/components/Modal";
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -12,7 +13,16 @@ const ModalProvider = () => {
   if (!isMounted) {
     return null;
   }
-  return <>Modals!</>;
+  return (
+    <Modal
+      title="Test Modal"
+      description="Test description"
+      isOpen
+      onChange={() => {}}
+    >
+      Test Children
+    </Modal>
+  );
 };
 
 export default ModalProvider;
