0149209c0598e0b319ddce6530a00bf81ca35162
gulifan <gulifan@dxy.cn>
2023-12-31 00:16:42 +0800

    feat: songItem 组件

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['zdcworotidgvkwbbelwi.supabase.co'],
+    }
+}
 
 module.exports = nextConfig
